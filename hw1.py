abb20530bd0975f60ca2b65812f45670455abcea
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-03-04 17:40:11 -0800

    add hw1.py

--- hw1.py
+++ hw1.py
@@ -8,7 +8,7 @@ import csv
 # Part. 2
 #=======================================
 # Read cwb weather data
-cwb_filename = '108061101.csv'
+cwb_filename = '108061124.csv'
 data = []
 header = []
 with open(cwb_filename) as csvfile:
@@ -25,106 +25,39 @@ with open(cwb_filename) as csvfile:
 # target_data = list(filter(lambda item: item['station_id'] == 'C0X260', data))
 
 # Retrive ten data points from the beginning.
-
 #target_data = data[:10]
 
+target = []
+stations = ['C0A880', 'C0F9A0','C0G640', 'C0R190', 'C0X260']
+#stations = ['C0A880', 'C0F9A0','C0G640', 'C0R190', 'C0X260', 'A']
+for station in stations:
+   target_data = list(filter(lambda item: item['station_id'] == station, data))
+   sum = 0.0
+   num = 0
+   for item in target_data:
+      if (item['PRES'] != '-99.000') and (item['PRES'] != '-999.000'):
+         sum += float(item['PRES'])
+         num += 1
+   if num > 0:
+      mean = sum/num
+      target.append([station, round(mean, 2)])
+   else:
+      target.append([station, 'None'])
 #=======================================
 
 # Part. 4
 #=======================================
 # Print result
 
-#print(target_data)
+#target.sort()
+print(sorted(target))
+#print(target)
 
 #========================================
 
-'''
-tmp = 0
-for item in data:
-   if (item['PRES'] == '-99.000'):
-      tmp += 1
-      print(item['station_id'], item['PRES'])
-print(tmp)
-'''
-
-target = []
-
-
-
-
-target_data = list(filter(lambda item: item['station_id'] == 'C0F9A0', data))
-sum = 0.0
-num = 0
-for item in target_data:
-   if item['PRES'] != '-99.000' and item['PRES'] != '-999.000':
-      sum += float(item['PRES'])
-      num += 1
-if num > 0:
-   mean = sum/num
-   target.append(['C0F9A0', round(mean, 2)])
-else:
-   target.append(['C0F9A0', 'None'])
-
-
-target_data = list(filter(lambda item: item['station_id'] == 'C0G640', data))
-sum = 0.0
-num = 0
-for item in target_data:
-   if item['PRES'] != '-99.000' and item['PRES'] != '-999.000':
-      sum += float(item['PRES'])
-      num += 1
-if num > 0:
-   mean = sum/num
-   target.append(['C0G640', round(mean, 2)])
-else:
-   target.append(['C0G640', 'None'])
-
-
-target_data = list(filter(lambda item: item['station_id'] == 'C0R190', data))
-sum = 0.0
-num = 0
-for item in target_data:
-   if item['PRES'] != '-99.000' and item['PRES'] != '-999.000':
-      sum += float(item['PRES'])
-      num += 1
-if num > 0:
-   mean = sum/num
-   target.append(['C0R190', round(mean, 2)])
-else:
-   target.append(['C0R190', 'None'])
-
-
-target_data = list(filter(lambda item: item['station_id'] == 'C0X260', data))
-sum = 0.0
-num = 0
-for item in target_data:
-   if item['PRES'] != '-99.000' and item['PRES'] != '-999.000':
-      sum += float(item['PRES'])
-      num += 1
-if num > 0:
-   mean = sum/num
-   target.append(['C0X260', round(mean, 2)])
-else:
-   target.append(['C0X260', 'None'])
 
 
 
 
 
-target_data = list(filter(lambda item: item['station_id'] == 'C0A880', data))
-sum = 0.0
-num = 0
-for item in target_data:
-   if (item['PRES'] != '-99.000') and (item['PRES'] != '-999.000'):
-      sum += float(item['PRES'])
-      num += 1
-if num > 0:
-   mean = sum/num
-   target.append(['C0A880', round(mean, 2)])
-else:
-   target.append(['C0A880', 'None'])
 
-#target.sort()
-print(sorted(target))
-print(target)
-# use for loop, please
